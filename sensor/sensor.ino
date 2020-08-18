41c18c67b768b05fc41fb809501df4494c64a3a2
Artem Bokhan <art@eml.ru>
2020-08-19 02:38:31 +0700

    Code update

--- sensor/sensor.ino
+++ sensor/sensor.ino
@@ -1,17 +1,22 @@
 #include <ArduinoSort.h> // https://github.com/emilv/ArduinoSort
 #include <LiquidCrystal.h>
+#include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
 
 #define triggerPin 1 // TX pin
 #define echoPin 3 // RX pin
-#define donePin 26
-#define measurements 21
+#define donePin 16
+#define measurements 12
 
-String version = "SENSOR-1.0";
+const String version = "SENSOR-1.0";
+const char*  ssid = "point-uz";
+const char*  key = "nou8haiy";
 
 int distance [measurements], median;
 
 // Init display
-LiquidCrystal lcd(12,13,17,16,27,14);
+//LiquidCrystal lcd(12,13,17,16,27,14); // espduino32
+LiquidCrystal    lcd(12,13,4,0,2,14);   // wemos r2
 
 void setup() {
     // Init timer pin
@@ -27,10 +32,12 @@ void setup() {
     lcd.clear();
     lcd.setCursor(0,0);
     lcd.print("Boot: " + version);
-    delay(500);
+    delay(300);
 }
 
 void loop(){
+    WiFi.begin(ssid, key);
+
     lcd.clear();
 
     // Measure distance
@@ -49,14 +56,14 @@ void loop(){
         digitalWrite(triggerPin, LOW);
 
         // recieve echo
-        distance[k] = pulseIn(echoPin, HIGH) * 0.034 / 2;
+        distance[k] = pulseIn(echoPin, HIGH, 50000) * 0.034 / 2;
 
         lcd.setCursor(11,0);
         lcd.print(String(k+1));
 
         lcd.setCursor(11,1);
         lcd.print(String(distance[k]) + "      ");
-        delay(50);
+        delay(10);
     }
 
     sortArray(distance, measurements);
@@ -66,13 +73,46 @@ void loop(){
     lcd.clear();
     lcd.print("Distance: " + String(median) + "cm");
 
-    delay(2000);
-    lcd.clear();
-    
-    lcd.clear();
-    lcd.print("Turning off");
+    for (int i=0; i<=500; i++) {
+        lcd.setCursor(0,1);
+        lcd.print("WiFi: " + String(WiFi.status()));
+        if (WiFi.status() == WL_CONNECTED) {
+            lcd.setCursor(0,1);
+            lcd.print("WiFi: connected       ");
+            delay(300);
+            break;
+        }
+        delay(50);
+    }
 
+    if (WiFi.status() != WL_CONNECTED) {
+        lcd.setCursor(0,1);
+        lcd.print("WiFi: failed     ");
+    }
+
+    else {
+        WiFiClient client;
+        HTTPClient http;
+ 
+        http.begin(client, "http://" + WiFi.gatewayIP().toString() + "/sensor?distance=" + String(median));
+
+        int httpCode = http.GET();
+
+        lcd.setCursor(0,1);
+        lcd.print("http: " + String(httpCode) + "          ");
+ 
+        http.end();
+    }
+    
+    WiFi.disconnect();
     delay(1000);
+    
+    lcd.setCursor(0,1);
+    lcd.print("Turning off     ");
+
+    delay(300);
 
     digitalWrite(donePin, HIGH); // Send OFF to timer
+
+    delay(10000); // debug
 }
